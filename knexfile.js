5bf07a701d4297a740e85dc30dc7a09fbbd8d305
CarnunMP <carnun@hotmail.co.uk>
2019-11-08 08:26:58 +0000

    removed comments from knex file

--- knexfile.js
+++ knexfile.js
@@ -11,11 +11,9 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
-    // needed when using foreign keys
     pool: {
       afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
   },
