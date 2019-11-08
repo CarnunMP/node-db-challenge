7a47b0bdaa1b332979481129de474bde0c67cd13
CarnunMP <carnun@hotmail.co.uk>
2019-11-08 09:31:06 +0000

    resources model fleshed out

---
+++ server/models/resourcesModel.js
@@ -0,0 +1,25 @@
+const db = require('../../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  add,
+}
+
+function find() {
+  return db('resources AS r')
+    .select('r.name', 'r.description');
+}
+
+function findById(id) {
+  return db('resources AS r')
+    .where({ 'r.id': id })
+    .first();
+}
+
+function add(resource) {
+  return db('resources AS r').insert(resource)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
