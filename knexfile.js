d9e870d34abe58b92b3e03d8b4d205939ddfbb64
CarnunMP <carnun@hotmail.co.uk>
2019-11-08 08:23:39 +0000

    added knexfile; initialsed eslint

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true, 
+    connection: {
+      filename: './data/database.db3',
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+  },
+};
